048fcb61b943124a5eb12eb0301caeccba64e02e
Amira <amirasaladin@hotmail.com>
2023-02-16 15:09:18 -0800

    Add files [5090]

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+
+/*
+ * Description of the function main
+ *
+ */
 int main(void)
 {
 	char str1[] = "\"Programming is like building a multilingual puzzle";
-	puts(str1);
+
+	puts (str1);
 	return(0);
 }

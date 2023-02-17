533a56d9fc40020c0425847a5c7a246ea7e151cc
Amira <amirasaladin@hotmail.com>
2023-02-16 17:11:05 -0800

    Add files [28344]

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19: Success");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19: ", 59);
 	return (0);
 }

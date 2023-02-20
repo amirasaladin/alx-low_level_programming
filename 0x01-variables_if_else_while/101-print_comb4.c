79aa41c34cbfee66b706b102bdd5fd5937337460
Amira <amirasaladin@hotmail.com>
2023-02-19 17:02:26 -0800

    Add files [49]

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,19 +10,21 @@ int main(void)
 {
 	int n = 0;
 	int n2 = 1;
+	int n3 = 2;
 	int num = 48;
 	int num2 = 49;
+	int num3 = 50;
 
 	while (n < 10)
 	{
 		while (n2 < 10)
 		{
-			putchar(num);
-			putchar(num2);
-			if (n != 8) 
+			while (n3 < 10)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(num);
+				putchar(num2);
+				n3++;
+				num3++;
 			}
 			n2++;
 			num2++;
@@ -31,6 +33,8 @@ int main(void)
 		num++;
 		n2 = n + 1;
 		num2 = num + 1;
+		n3 = n2 + 1;
+		num3 = num2 + 1;
 	}
 	putchar('\n');
 	return (0);

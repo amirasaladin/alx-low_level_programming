d21d0a2b9f70844bce23628c90d871a93bdba50d
Amira <amirasaladin@hotmail.com>
2023-02-21 13:59:26 -0800

    Add files [27358]

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
  * Return: Always 0 (Success)
 */
 
-int main (void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -16,5 +16,6 @@ int main (void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 	return (0);
 }

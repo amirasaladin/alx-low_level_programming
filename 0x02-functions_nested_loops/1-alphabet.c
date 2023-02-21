2492772b33850296b3c7a895eaa572199fbaa318
Amira <amirasaladin@hotmail.com>
2023-02-21 14:27:52 -0800

    Add files [8148]

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
-/*
- * print_alphabet - writes the whole alphabet in small letters
- * Return (void).
- */
+/**
+* print_alphabet - check the code
+*
+* Return: Always void.
+*/
 void print_alphabet(void)
 {
 char alpha = 'a';

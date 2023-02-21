c7684b25977bcd78998343b7fbbe0427e951cdbe
Amira <amirasaladin@hotmail.com>
2023-02-21 14:54:28 -0800

    Add files [3050]

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return: void
+*/
+
+void print_alphabet_x10 (void)
+{
+	while (count < 10)
+	{
+		print_alphabet();
+	}
+	return;
+
+}

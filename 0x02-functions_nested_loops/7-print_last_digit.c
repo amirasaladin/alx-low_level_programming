06bc6d4a082915982094d24a7703439dc6f3aa70
Amira <amirasaladin@hotmail.com>
2023-02-21 17:13:20 -0800

    Add files [10918]

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,7 +12,9 @@ int print_last_digit(int n)
 	int last_digit;
 
 	last_digit = n % 10;
-/*	twice = last_digit * 10 + last_digit; */
-
-	return (printf("%d%d\n", last_digit, last_digit));
+	twice = last_digit * 10 + last_digit;
+	_putchar(last_digit);
+	_putchar(last_digit);
+	_putchar('\n');
+	return (0);
 }

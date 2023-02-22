a43ff97283afe043e5ad6a0420006ef9b2f8c4ea
Amira <amirasaladin@hotmail.com>
2023-02-21 16:07:17 -0800

    Add files [25676]

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * main - Entry point
- *
+ * print_sign - Check the code
+ * @n
  * Return: Always 0 (Success)
 */
 
-int print_sign (int n)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if ( n < 0)
+	else if( n < 0)
 	{
 		_putchar('-');
 		return (-1);

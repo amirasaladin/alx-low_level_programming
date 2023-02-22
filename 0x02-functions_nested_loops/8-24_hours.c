42eb0cf36eea6ea9edc2036b6b49784454468115
Amira <amirasaladin@hotmail.com>
2023-02-22 08:56:51 -0800

    Add files [24489]

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,18 +7,28 @@
 
 void jack_bauer(void)
 {
-	int hours = 0;
-	int minutes = 0;
-	while (hours < 24)
+	int hr = 0;
+	int min = 0;
+
+	while (hr < 24)
 	{
-		while (minutes < 60)
+		while (min < 60)
 		{
-			if (minutes < 10)
+			if (hr < 10)
 			{
-				_putchar(0);
-				_putchar(48 + minutes);
+				_putchar('0');
 			}
+			_putchar(hr + '0');
+			_putchar(':');
+			if (min < 10)
+			{
+				_putchar('0');
+			}
+			_putchar(min + '0');
+			_putchar('\n');
+			min++;
 		}
+		hr++;
 	}
 	return;
 

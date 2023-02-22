a60f2e5c14d0daa00ed60054c308366560a173d3
Amira <amirasaladin@hotmail.com>
2023-02-21 16:24:16 -0800

    Add files [9576]

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,14 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * name - Check code
+ * _abs - Check code
+ *
  * @n - int type argument - integer
+ *
  * Return: the return value data
 */
 
-int _abs (int n)
+int _abs(int n)
 {
-	return abs(n);
+	return (abs(n));
 }

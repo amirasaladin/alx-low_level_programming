50e3ba668f5e58cf26b5e13a368aa9dd051fc63a
Amira <amirasaladin@hotmail.com>
2023-02-21 16:33:08 -0800

    Add files [27298]

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * print_last_digit - Check the code
+ * @n: digit
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int n)
+{
+	int n;
+	int last_digit;
+
+	last_digit = n % 10;
+	return (last_digit);
+}

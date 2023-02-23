65a3bf2d913170ff21a093fb062472d0f78dfb19
Amira <amirasaladin@hotmail.com>
2023-02-22 17:15:24 -0800

    Add files [25061]

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,22 +8,28 @@
 
 void print_to_98(int n)
 {
-	int fdigit, ldigit;
+	int fdigit, mdigit, ldigit, absn;
 
 	while (n != 98)
 	{
 		if (n < 0)
 		{
-			n = abs(n);
+			absn = abs(n);
 			_putchar('-');
+			fdigit = absn * 0.1;
+			mdigit = (absn - 100) * 0.1;
+			ldigit = absn % 10;
 		}
 		else
 		{
 			fdigit = n * 0.1;
+			mdigit = (n - 100) * 0.1;
 			ldigit = n % 10;
 		}
 		if (fdigit != 0)
 			_putchar(fdigit + '0');
+		if (n >= 100)
+			_putchar(mdigit + '0');
 		_putchar(ldigit + '0');
 		if(n != 98)
 		{

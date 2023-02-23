726b02797f3eeeeec305a830e40588c0526038a5
Amira <amirasaladin@hotmail.com>
2023-02-22 17:27:04 -0800

    Add files [7480]

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,13 +1,38 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * name - Check code
- * @n - integer
+ * times_table - Check code
  * Return: the return value data
 */
 
-int name(int)
+void times_table(int n)
 {
-	_putchar();
-	return;
+	int x = 0;
+	int y = 0;
+	int fdigit, ldigit, mult;
 
+	while (x <= n && 0 < n < 15)
+	{
+		while (y <= n)
+		{
+			mult = x * y;
+			fdigit = mult * 0.1;
+			ldigit = mult % 10;
+			if (fdigit != 0)
+				_putchar(fdigit + '0');
+			if (fdigit == 0 && y != 0)
+				_putchar(' ');
+			_putchar(ldigit + '0');
+			if (y != 9)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+			y++;
+		}
+		_putchar('\n');
+		y = 0;
+		x++;
+	}
+	return;
 }

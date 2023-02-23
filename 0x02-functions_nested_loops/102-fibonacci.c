9778d11b94126c94d197e786a365a7d6e65d4752
Amira <amirasaladin@hotmail.com>
2023-02-22 18:33:06 -0800

    Add files [8477]

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * fib - Check code
+ *
+ * Return: the return value data
+*/
+
+void fib(void)
+{
+	int x, y, n, count;
+	count = 0;
+	x = 1;
+	y = 2;
+	n = x + y;
+	while (count < 51)
+	{
+		if (count != 50)
+			printf("%d,", n);
+		else
+			printf("%d\n", n);
+		x = y;
+		y = n;
+		count++;
+	}
+}
+
+/**
+ * main - check code
+ *
+ * Return: int
+ */
+
+int main(void)
+{
+	fib();
+	return (0);
+}

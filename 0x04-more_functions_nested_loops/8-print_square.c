a0ada7b87df4fbd3443373c2b60401acf2854be0
Amira <amirasaladin@hotmail.com>
2023-02-23 18:33:33 -0800

    Add files [1667]

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_squares - check the code
+*
+* @size: size of square
+*
+* Return: Always void.
+*/
+void print_squares(int size)
+{
+int i = 0;
+int j = 0;
+
+	while (i <= size && size >= 0)
+	{
+		while (j <= size)
+		{
+			_putchar('#');
+			j++;
+		}
+		i++;
+		j = 0;
+		_putchar('\n');
+	}
+}

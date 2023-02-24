177242acc954be4488876bf42867be93cba7a42b
Amira <amirasaladin@hotmail.com>
2023-02-23 17:51:40 -0800

    Add files [14439]

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* print_numbers - check the code
+* more_numbers - check the code
 *
 * Return: Always void.
 */
-void print_numbers(void)
+void more_numbers(void)
 {
 int i = 0;
 int j = 0;

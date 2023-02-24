262fed8d4b7a51f1482a8ceb9820093edab8a8e7
Amira <amirasaladin@hotmail.com>
2023-02-23 18:43:56 -0800

    Add files [14342]

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,15 +12,15 @@ void print_square(int size)
 int i = 0;
 int j = 0;
 
-	while (i < size && size >= 0)
+	while (i <= size && size >= 0)
 	{
 		while (j < size)
 		{
 			_putchar('#');
 			j++;
 		}
+		_putchar('\n');
 		i++;
 		j = 0;
-		_putchar('\n');
 	}
 }

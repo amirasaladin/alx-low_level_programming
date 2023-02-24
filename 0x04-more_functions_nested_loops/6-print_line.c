82ad1f54282a7aab5b50945fd32ce7d1397dda6d
Amira <amirasaladin@hotmail.com>
2023-02-23 18:17:11 -0800

    Add files [20458]

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_line - check the code
+*
+* @n: integer
+*
+* Return: Always void.
+*/
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		_putchar('_');
+		i++;
+	}
+}
+

1b321197537c1f1b443221ac8be4d0061a3ea185
Amira <amirasaladin@hotmail.com>
2023-02-24 20:10:11 -0800

    Add files [2500]

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * lprime_factor - Check code
+ *
+ * Return: the return value data
+*/
+void lprime_factor(void)
+{
+	long int n = 612852475143;
+	long int i = 2;
+
+	while (n != i)
+	{
+		if (n % i == 0)
+		{
+			n = n / i;
+			/*printf("%ldn, ", n);*/
+			/*printf("%ldi, ", i);*/
+		}
+
+		if (n % 2 == 0)
+		{
+			i = 2;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	printf("%ld \n", n);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	lprime_factor();
+	return (0);
+}

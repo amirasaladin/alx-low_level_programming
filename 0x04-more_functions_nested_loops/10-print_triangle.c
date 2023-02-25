b3d82dfa2db31d06c69b7ee4e57e4108f475bcad
Amira <amirasaladin@hotmail.com>
2023-02-24 18:14:41 -0800

    Add files [30038]

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_triangle - Check code
+ * @size: integer
+ * Return: the return value data
+*/
+
+void print_triangle(int size)
+{
+	int count = 1;
+	int i = size;
+	int j = size - count;
+	int k = 0;
+
+	while (i > 0)
+	{
+		while (j > 0)
+		{
+			_putchar('.');
+			j--;
+		}
+		count++;
+		j = size - count;
+		while (k < count - 1)
+		{
+			_putchar('#');
+			k++;
+		}
+		k = 0;
+		_putchar('\n');
+		i--;
+	}
+	return;
+
+}

fcb0fc5b5a2c303acc6b0fd4138ee6ede5cf01ea
Amira <amirasaladin@hotmail.com>
2023-02-26 15:24:28 -0800

    Add files [25554]

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - Check code
+ * @n: integer
+ * Return: the return value data
+*/
+
+void print_number(int n)
+{
+	int ldigit;
+
+	if (n == 0)
+	{
+		n = 0;
+	}
+	else
+	{
+		ldigit = n % 10;
+		print_number(n * 0.1);
+		putchar(ldigit + '0');
+	}
+}
+
+/*int main(void) {print_number(1024); return(0);}*/

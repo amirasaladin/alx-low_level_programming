891e74f0da780802a020f1c1a3a949fece3fd070
Amira <amirasaladin@hotmail.com>
2023-02-27 16:03:27 -0800

    Add files [2184]

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - Check code
+ * @s: integer
+ * Return: the return value data
+*/
+
+void print_rev(char *s)
+{
+	int slen;
+
+	slen = strlen(s) - 1;
+	while (slen >= 0)
+	{
+	_putchar(s[slen])
+	slen--;
+	}
+}

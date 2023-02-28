02dd096b24fc53b70b72c5c51e4bd96dea3c85bf
Amira <amirasaladin@hotmail.com>
2023-02-27 17:59:15 -0800

    Add files [30326]

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - Check code
+ * @src: integer
+ * Return: the return value data
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	return strcpy(src, dest);
+}

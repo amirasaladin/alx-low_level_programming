f1c7c5f4d3a08d49703c792aa82d5f769da33e7c
Amira <amirasaladin@hotmail.com>
2023-02-27 17:45:55 -0800

    Add files [10385]

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,15 +2,17 @@
 #include <stdio.h>
 /**
  * print_array - Check code
- * @n - integer
+ * @a: array
+ * @n: length of a
  * Return: the return value data
 */
 
 void print_array(int *a, int n)
 {
 	int i = 0;
+
 	while (i < n)
-	{	
+	{
 		printf("%d", a[i]);
 		i++;
 		if (i != n)

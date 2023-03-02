e303ed838f057f99c2c31349f398569679daa469
Amira <amirasaladin@hotmail.com>
2023-03-02 15:55:10 -0800

    Add files [5365]

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - Check code
+ * @dest: string
+ * @src: string
+ * Return: the return value data
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}

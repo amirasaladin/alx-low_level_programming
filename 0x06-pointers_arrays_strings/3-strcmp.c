9655e6a10d355554553c2217bdbac8c339eee3fa
Amira <amirasaladin@hotmail.com>
2023-03-02 16:18:43 -0800

    Add files [9366]

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - Check code
+ * @s1: string
+ * @s2: string
+ * Return: the return value data
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}

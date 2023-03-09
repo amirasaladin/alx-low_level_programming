9a46946ead8ace15eaba56565c50487f7b6608fc
Amira <amirasaladin@hotmail.com>
2023-03-08 22:57:02 -0800

    Add files [29922]

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * factorial - Check code
+ * @n: integer
+ * Return: the return value data
+*/
+
+int factorial(int n)
+{
+	if (n < 0)
+		return -1;
+	else if (n == 0 || n == 1)
+		return 1;
+	else
+		return (n * factorial(n - 1));
+}

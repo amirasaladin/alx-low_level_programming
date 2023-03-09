c7040aba53945c2165032c635b4d39f6c82ba46f
Amira <amirasaladin@hotmail.com>
2023-03-08 22:39:42 -0800

    Add files [24052]

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * strlen_recursion - Check code
+ * @s: string
+ * Return: the return value data
+*/
+
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}

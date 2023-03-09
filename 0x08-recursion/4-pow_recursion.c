ee4d745d33f79c908a4e19597d8bd622010e356c
Amira <amirasaladin@hotmail.com>
2023-03-09 11:42:44 -0800

    Add files [18540]

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - Check code
+ * @x: integer
+ * @y: integer
+ * Return: the return value data
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (x == 0)
+		return 1;
+	else
+		return (x * _pow_recursion(x, y - 1));
+}

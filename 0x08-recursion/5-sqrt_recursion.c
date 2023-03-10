e9af6cdf8435e952c95a89b118bf26468e03f735
Amira <amirasaladin@hotmail.com>
2023-03-10 00:50:25 -0800

    Add files [31333]

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,35 @@
 #include "main.h"
-#include <math.h>
+/**
+* good_enough - check the code
+* @n: the number you're looking for its sqrt
+* @m: guess
+* Return: 1, 0, -1
+*/
+int good_enough(int n, int m)
+{
+	if (m * m == n)
+		return (1);
+	else if (m > n)
+		return (-1);
+	else
+		return (0);
+}
+
+/**
+ * _sqrt_recursion_iter - check the code
+ * @n: the number you're looking for its sqrt
+ * @m: guess
+ * Return: m, -1
+*/
+int _sqrt_recursion_iter(int n, int m)
+{
+	if (good_enough(n, m) == -1 || n < 0)
+		return (-1);
+	else if (good_enough(n, m))
+		return (m);
+	else
+		return (_sqrt_recursion_iter(n, m + 1));
+}
 /**
  *  _sqrt_recursion - Check code
  * @n: integer
@@ -8,5 +38,5 @@
 
 int _sqrt_recursion(int n)
 {
-	return (sqrt(n));
+	return (_sqrt_recursion_iter(n, 1));
 }

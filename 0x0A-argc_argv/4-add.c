ffd5c2a553a5110a48917e5efab495dff8ca7a6d
Amira <amirasaladin@hotmail.com>
2023-03-11 06:55:14 -0800

    Add files [15952]

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Check code
+ * @argc: count
+ * @argv: vector array
+ * Return: the return value data
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (isdigit(argv[i][0]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}

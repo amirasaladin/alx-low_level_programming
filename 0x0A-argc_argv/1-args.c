335b6aed48821be5579e0a0d7f9465f7b9b8bc00
Amira <amirasaladin@hotmail.com>
2023-03-11 05:03:50 -0800

    Add files [25449]

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Check code
+ * @argc: count
+ * @argv: vector array
+ * Return: the return value data
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}

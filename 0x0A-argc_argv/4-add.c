9158da45d0f894bc2ecb43ee0792a8ea2776a0dc
Amira <amirasaladin@hotmail.com>
2023-03-11 08:30:09 -0800

    Add files [18077]

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,13 +15,12 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (isdigit(argv[i][0]) == 0 || argc != 3)
+		if (isdigit(argv[i][0]) == 0 && argc > 1)
 		{
 			printf("Error\n");
 			return (1);
 		}
-		if (argc < 3 || argc > 3)
-			sum += atoi(argv[i]);
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);

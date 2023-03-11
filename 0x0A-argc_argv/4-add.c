bdb4650ac05d40de1ef4329d04ed72d3aacf63a3
Amira <amirasaladin@hotmail.com>
2023-03-11 08:32:04 -0800

    Add files [3826]

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (isdigit(argv[i][0]) == 0 && argc != 3)
+		if (isdigit(argv[i][0]) == 0)
 		{
 			printf("Error\n");
 			return (1);

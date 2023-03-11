ae93ef6a000b035ae5e3bc3544141334fcf392cd
Amira <amirasaladin@hotmail.com>
2023-03-11 04:59:14 -0800

    Add files [2810]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Check code
+ * @argc: count
+ * @argv: vector array
+ * Return: the return value data
+*/
+
+int main (int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}

e1dab4d93035d535fe0b4d649e8409214c7928bf
Amira <amirasaladin@hotmail.com>
2023-03-14 12:03:09 -0700

    Add files [9085]

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 	{
 		ptr = malloc(INT_MAX);
-		return (NULL);
+		if (ptr == NULL)
+			return (NULL);
 	}
 	for (i = 0; i < size; i++)
 	{

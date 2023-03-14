787d8b4b7ab6d08f4e41419685749bd6e3a2a586
Amira <amirasaladin@hotmail.com>
2023-03-14 14:55:19 -0700

    Add files [5893]

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,20 +11,20 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j;
+	unsigned long int i;
+	unsigned long int j;
 	char *s3;
+
 	if (!s1 || !s2)
 		return (NULL);
-	int size = sizeof(char) * (strlen(s1) + strlen(s2) + 1);
-	s3 = malloc(size);
+	s3 = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
 	for (i = 0; i < strlen(s1); i++)
-	{
 		s3[i] = s1[i];
-		for (j = 0; j < strlen(s2); j++)
-		{
-			s3[j] = s2[j];
-		}
+	for (j = 0; j < strlen(s2); j++)
+	{
+		s3[i] = s2[j];
+		i++;
 	}
-	s3[size - 1] = '\0';
+	s3[i] = '\0';
 	return (s3);
 }

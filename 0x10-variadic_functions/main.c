25c1738f72e19ddbfdc913c3b3f96eac3d427099
Amira <amirasaladin@hotmail.com>
2023-03-23 16:50:03 -0700

    Add files [14371]

--- 0x10-variadic_functions/main.c
+++ 0x10-variadic_functions/main.c
@@ -1,5 +1,5 @@
 #include "variadic_functions.h"
-
+#include <stddef.h>
 /**
  * main - check the code
  *
@@ -8,5 +8,6 @@
 int main(void)
 {
     print_numbers(", ", 4, 0, 98, -1024, 402);
+    print_numbers(NULL, 2, 2, 1);
     return (0);
 }

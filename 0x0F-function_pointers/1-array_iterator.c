b914608d51f5f7e09f7d901750862200f18f7082
Amira <amirasaladin@hotmail.com>
2023-03-23 11:39:04 -0700

    Add files [22838]

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - Check code
+ * @array: array
+ * @size: size of the array
+ * @action: pointer to the function you need to use
+ * Return: the return value data
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}

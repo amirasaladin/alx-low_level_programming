eb018051fb33feb96863d0c45446ee9e7ff5090e
Amira <amirasaladin@hotmail.com>
2023-03-23 15:26:41 -0700

    Add files [4291]

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - Check code
+ * @n: integer
+ * Return: the return value data
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	va_list args;
+	int i;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_args(args, int);
+	}
+	va_end(args);
+	return sum;
+}

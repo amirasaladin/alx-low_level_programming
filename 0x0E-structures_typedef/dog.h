a19bd6c8db14d7640571b2140c035bcd1b74633e
amira <amirasaladin@hotmail.com>
2023-05-03 05:20:46 -0700

    Add files 24653

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct Dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct Dog
+{
+char *name;
+float *age;
+char *owner;
+};
+#endif

ce6ab4a304d63492719ecc5b352772cb6ac12f3e
amira <amirasaladin@hotmail.com>
2023-05-03 09:09:50 -0700

    Add files 16666

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,11 +1,13 @@
+#include "lists.h"
 #include <stdio.h>
+#include <stddef.h>
 /**
  * print_list - a function that prints all the elements of a list_t list
  * @h: pointer to the list
  * Return: the number of nodes
  */
 
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
 {
 	size_t i;
 
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS
 #define LISTS
+#include <stddef.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)

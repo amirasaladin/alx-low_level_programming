d886fa128cf2274170f7f05d8bcf0adb1c264f61
amira <amirasaladin@hotmail.com>
2023-05-03 00:17:56 -0700

    Add files 29300

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _realloc - Check code
+ * @ptr: pointer
+ * @old_size: 
+ * Return: the return value data
+*/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+}

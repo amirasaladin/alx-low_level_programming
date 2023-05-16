a2a9e43c33ffb183ad6b40a1e2331b30a1ace21f
amira <amirasaladin@hotmail.com>
2023-05-16 12:54:14 -0700

    Add files 9502

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,8 +11,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, w_bytes;
-	int count = strlen(text_content);
+	int fd, w_bytes, count;
 
 	if (!filename)
 		return (-1);
@@ -21,12 +20,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
+	if (!text_content)
 	{
 		close(fd);
 		return (1);
 	}
 
+	count = strlen(text_content);
 	w_bytes = write(fd, text_content, count);
 	if (w_bytes == -1)
 	{

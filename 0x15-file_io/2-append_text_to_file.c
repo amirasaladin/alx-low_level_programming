21844cee5f642a61729a36eb618d3ee65657a328
amira <amirasaladin@hotmail.com>
2023-05-16 12:36:14 -0700

    Add files 7600

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
+	if (text_content == NULL)
 	{
 		close(fd);
 		return (1);

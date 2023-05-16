e413a725049d54cc4a22dc92e7ced4a6480feef0
amira <amirasaladin@hotmail.com>
2023-05-16 09:18:33 -0700

    Add files 656

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,6 +23,13 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+	
+
 	for (count = 0; text_content[count]; count++)
 		;
 

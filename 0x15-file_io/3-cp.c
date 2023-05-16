d62f8bb10d6a65e961eeeddddb9b41a0c9d07da7
amira <amirasaladin@hotmail.com>
2023-05-16 14:48:39 -0700

    Add files 5330

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- * main - check the code
- *
+ * main - entry point
+ * descripton: copies the content of a file to another file.
+ * @ac: number of arguments
+ * @av: array of arguments
  * Return: Always 0.
  */
 int main(int ac, char **av)
@@ -10,30 +12,19 @@ int main(int ac, char **av)
 	char buf[1024];
 
 	if (ac != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
 
 	file_from_d = open(av[1], O_RDONLY);
 	if (file_from_d == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), 
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+
 	file_to_d = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 00664);
 	if (file_to_d == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 
 	r_bytes = read(file_from_d, buf, 1024);
 	if (r_bytes == -1)
-	{
-		dprintf( 2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
 	if (r_bytes > 0)
 	{
 		w_bytes = write(file_to_d, buf, r_bytes);

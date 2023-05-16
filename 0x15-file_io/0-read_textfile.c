aa1873b53461b22b1cba238a5b5daf5fa45e139e
amira <amirasaladin@hotmail.com>
2023-05-16 07:09:33 -0700

    Add files 20900

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: file name
+ * @letters: the number of letters it should read and print
+ * Return: the actual number of letters it could read and print
+ * if the file can not be opened or read, return 0
+ * if filename is NULL return 0
+ * if write fails or does not write the expected amount of bytes, return 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t r_bytes, w_bytes;
+
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer || !filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	/* open fail return value of -1 */
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	r_bytes = read(fd, buffer, letters);
+	if (r_bytes == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	
+	w_bytes = write(STDOUT_FILENO, buffer, r_bytes);
+	if (w_bytes == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	
+	free(buffer);
+	close(fd);
+	return (w_bytes);
+}
